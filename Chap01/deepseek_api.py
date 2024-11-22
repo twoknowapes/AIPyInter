d423ca08331b2e3f48b9fe25a83cf65875f840c8
twoknowapes <1007845131@qq.com>
2024-11-22 16:26:22 +0800

    Create deepseek_api.py

---
+++ Chap01/deepseek_api.py
@@ -0,0 +1,19 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="sk-your-key",
+    base_url="https://api.deepseek.com/v1"
+)
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
